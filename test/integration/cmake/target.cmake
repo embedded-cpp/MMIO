061a1843072a43cb4d5c6319c91e5d1a7af99aa7
RoddyRod12 <roddyrod12@proton.me>
2026-02-08 17:02:27 +0100

    Change cmake-format tabsize

--- test/integration/cmake/target.cmake
+++ test/integration/cmake/target.cmake
@@ -1,9 +1,9 @@
 function(mcu_add_executable)
-  add_executable(${ARGV})
-  target_link_libraries(${ARGV0} PRIVATE stm32f401_flags)
+    add_executable(${ARGV})
+    target_link_libraries(${ARGV0} PRIVATE stm32f401_flags)
 endfunction()
 
 function(mcu_add_library)
-  add_library(${ARGV})
-  target_link_libraries(${ARGV0} PRIVATE stm32f401_flags)
+    add_library(${ARGV})
+    target_link_libraries(${ARGV0} PRIVATE stm32f401_flags)
 endfunction()
