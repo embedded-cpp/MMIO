181e338d55cd7a5a22be5136ad3837fcf1b1a313
roddyrod12 <roddyrod12@proton.me>
2026-07-21 16:01:20 +0200

    Some fixes

--- test/integration/source/mcu/stm32f401re/startup.s
+++ test/integration/source/mcu/stm32f401re/startup.s
@@ -1,69 +1,25 @@
-/**
-  ******************************************************************************
-  * @file      startup_stm32f401xe.s
-  * @author    MCD Application Team
-  * @brief     STM32F401xExx Devices vector table for GCC based toolchains. 
-  *            This module performs:
-  *                - Set the initial SP
-  *                - Set the initial PC == Reset_Handler,
-  *                - Set the vector table entries with the exceptions ISR address
-  *                - Branches to main in the C library (which eventually
-  *                  calls main()).
-  *            After Reset the Cortex-M4 processor is in Thread mode,
-  *            priority is Privileged, and the Stack is set to Main.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2017 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-    
+
   .syntax unified
   .cpu cortex-m4
   .fpu softvfp
   .thumb
-
 .global  g_pfnVectors
 .global  Default_Handler
 
-/* start address for the initialization values of the .data section. 
-defined in linker script */
 .word  _sidata
-/* start address for the .data section. defined in linker script */  
 .word  _sdata
-/* end address for the .data section. defined in linker script */
 .word  _edata
-/* start address for the .bss section. defined in linker script */
 .word  _sbss
-/* end address for the .bss section. defined in linker script */
 .word  _ebss
-/* stack used for SystemInit_ExtMemCtl; always internal RAM used */
 
-/**
- * @brief  This is the code that gets called when the processor first
- *          starts execution following a reset event. Only the absolutely
- *          necessary set is performed, after which the application
- *          supplied main() routine is called. 
- * @param  None
- * @retval : None
-*/
 
-    .section  .text.Reset_Handler
-  .weak  Reset_Handler
-  .type  Reset_Handler, %function
+
+.section  .text.Reset_Handler
+.weak  Reset_Handler
+.type  Reset_Handler, %function
+
 Reset_Handler:  
   ldr   sp, =_estack    		 /* set stack pointer */
-
-/* Call the clock system initialization function.*/
-  bl  SystemInit  
-
-/* Copy the data segment initializers from flash to SRAM */  
   ldr r0, =_sdata
   ldr r1, =_edata
   ldr r2, =_sidata
@@ -80,7 +36,6 @@ LoopCopyDataInit:
   cmp r4, r1
   bcc CopyDataInit
   
-/* Zero fill the bss segment. */
   ldr r2, =_sbss
   ldr r4, =_ebss
   movs r3, #0
@@ -93,34 +48,20 @@ FillZerobss:
 LoopFillZerobss:
   cmp r2, r4
   bcc FillZerobss
- 
-/* Call static constructors */
-    bl __libc_init_array
-/* Call the application's entry point.*/
+
+  bl __libc_init_array
+
   bl  main
   bx  lr    
+
 .size  Reset_Handler, .-Reset_Handler
 
-/**
- * @brief  This is the code that gets called when the processor receives an 
- *         unexpected interrupt.  This simply enters an infinite loop, preserving
- *         the system state for examination by a debugger.
- * @param  None     
- * @retval None       
-*/
-    .section  .text.Default_Handler,"ax",%progbits
+.section  .text.Default_Handler,"ax",%progbits
 Default_Handler:
 Infinite_Loop:
   b  Infinite_Loop
   .size  Default_Handler, .-Default_Handler
-/******************************************************************************
-*
-* The minimal vector table for a Cortex M3. Note that the proper constructs
-* must be placed on this to ensure that it ends up at physical address
-* 0x0000.0000.
-* 
-*******************************************************************************/
-   .section  .isr_vector,"a",%progbits
+  .section  .isr_vector,"a",%progbits
   .type  g_pfnVectors, %object
     
 g_pfnVectors:
@@ -231,13 +172,6 @@ g_pfnVectors:
 
   .size  g_pfnVectors, .-g_pfnVectors
 
-/*******************************************************************************
-*
-* Provide weak aliases for each Exception handler to the Default_Handler. 
-* As they are weak aliases, any function with the same name will override 
-* this definition.
-* 
-*******************************************************************************/
    .weak      NMI_Handler
    .thumb_set NMI_Handler,Default_Handler
   
