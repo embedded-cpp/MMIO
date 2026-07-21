0b6222ab696402c6b65172995c4bbf5b29c24eac
roddyrod12 <roddyrod12@proton.me>
2026-07-21 16:28:49 +0200

    Not need to note this is internal

--- include/mmio/mmio.hpp
+++ include/mmio/mmio.hpp
@@ -69,7 +69,6 @@ namespace mmio {
         /**
          * @brief Internal trait: true if field F belongs to this register.
          * @tparam F The field type to check.
-         * @note This is an internal implementation detail and is not part of the public API.
          */
         template <typename F>
         static constexpr bool BELONGS_TO_THIS_V = requires {
